8458df949c78cf9279d188a45842f6f2f48786e1
Victor-Ritseson <vritseson@gmail.com>
2021-03-07 23:19:56 +0100

    Recreated Stack class

---
+++ Stack22/src/SimpleStack.java
@@ -0,0 +1,107 @@
+
+
+public class SimpleStack<E> {
+	Node<E> first;
+	int size;
+	
+	
+	public SimpleStack(){
+		first = null;
+		size = 0;
+	}
+	
+	public E peek() {
+		if(first == null) {
+			return null;
+		} else {
+			return first.element;
+		}
+	}
+	
+	public E pop() {
+		if(first == null) {
+			return null;
+		} else {
+			E temp = first.element;
+			first = first.next;
+			return temp;
+		}
+	}
+	
+	
+	public E push(E item) {
+		Node<E> newItem = new Node<E>(item);
+		if(first == null) {
+			first = newItem;
+		} else {
+			Node<E> temp = first;
+			
+			while(temp != null) {
+				if(temp.next == null) {
+					temp.next = newItem;
+					break;
+				} 
+				temp = temp.next;
+			}
+		}
+		size++;
+		return newItem.element;
+	}
+	
+	
+	public int size() {
+		return size;
+	}
+	
+	public int search(Object o) {
+		if(first == null) {
+			return -1;
+		}
+		
+		Node<E> temp = first;
+		
+		int i = 1;
+		while(temp != null) {
+			if(temp.element.equals((E)o)) {
+				return i;
+			}
+			temp = temp.next;
+			i++;
+		}
+		
+		
+		return -1;
+	}
+	
+	public static void main(String args[]) {
+		SimpleStack<String> et = new SimpleStack<String>();
+		et.push("hejsan");
+		et.push("hejdå");
+		et.push("ska vara avstånd 3");
+		
+		System.out.print(et.size);
+		System.out.print(et.search("ska vara avstånd 3"));
+		System.out.print(et.pop());
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	private static class Node<E> {
+		E element;
+		Node<E> next;
+
+		/**
+		 * Creates a new Node
+		 * @param x the char which is set as the Node's data
+		 */
+		private Node(E x) {
+			element = x;
+			next = null;
+		}
+}
+}
